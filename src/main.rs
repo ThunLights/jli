46e4d6e5340dcf77f776709ed23dd3ceeec3dcc8
ROBOTofficial <tkata12@outlook.jp>
2024-09-30 22:47:24 -0400

    public/のファイルをインポートするようにコード変更

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-
 use jli::utils::{ip::{header2ip, tor_check}, json::struct2json};
 
-use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_files::Files;
+use actix_web::{get, post, web::Redirect, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -11,8 +11,8 @@ struct TestStruct {
 	content: String,
 }
 
-#[get("/")]
-async fn main_page(req: HttpRequest) -> impl Responder {
+#[post("/api/test")]
+async fn test_api(req: HttpRequest) -> impl Responder {
 	let ip = header2ip(&req).await;
 	let tor = tor_check(&req).await;
 	let contents = TestStruct {
@@ -24,11 +24,18 @@ async fn main_page(req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().content_type("application/json").body(struct2json(&contents).await.unwrap())
 }
 
+#[get("/")]
+async fn main_page() -> impl Responder {
+	Redirect::to("/index.html").permanent()
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(main_page)
+			.service(main_page)
+            .service(test_api)
+			.service(Files::new("/", "public/").prefer_utf8(true))
     })
     .bind(("0.0.0.0", 3090))
     .expect("fn main crashed")
