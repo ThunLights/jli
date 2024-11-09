0410459362c25691f798c0fa92ee5b4ed6b14ae8
ROBOT <tkata12@outlook.jp>
2024-11-10 06:51:32 +0900

    サーバーサイド側でのhostnameチェックを導入

--- src/utils/url.rs
+++ src/utils/url.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use url::Url;
 
 pub async fn url_format_check(url: &str) -> bool {
     if let Ok(re) = Regex::new(r"^https?:\/\/[\w/:%#\$&\?\(\)~\.=\+\-]+$") {
@@ -6,4 +7,12 @@ pub async fn url_format_check(url: &str) -> bool {
     }
 
     false
+}
+
+pub fn domain_check(url: &str) -> bool {
+	if let Ok(issue_list_url) = Url::parse(url) {
+		return issue_list_url.host_str() == Some("jli.li");
+	}
+
+	false
 }
