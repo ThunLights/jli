25cbb5376699b4829bbdcca9debb6d27dcf96e7f
ROBOT <tkata12@outlook.jp>
2024-11-23 21:37:44 +0900

    add character_limit_check fn

--- src/utils/url.rs
+++ src/utils/url.rs
@@ -1,12 +1,18 @@
 use regex::Regex;
 use url::Url;
 
+const MAX_URL_LENGTH: usize = 2048;
+
 pub async fn url_format_check(url: &str) -> bool {
     if let Ok(re) = Regex::new(r"^https?:\/\/[\w/:%#\$&\?\(\)~\.=\+\-]+$") {
         return re.is_match(url);
     }
 
     false
+}
+
+pub fn character_limit_check(url: &str) -> bool {
+	return url.len() > MAX_URL_LENGTH;
 }
 
 pub fn domain_check(url: &str) -> bool {
