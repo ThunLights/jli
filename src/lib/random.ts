36c1db6ae34bb04c6702bf0fd256e6704db18ee3
ROBOT <tkata12@outlook.jp>
2025-02-20 21:16:53 +0900

    formattingを実行

--- src/lib/random.ts
+++ src/lib/random.ts
@@ -1,2 +1,7 @@
-
-export const randomString = (n: number, s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"): string => Array.from(Array(n)).map(() => s[Math.floor(Math.random()*s.length)]).join("");
+export const randomString = (
+	n: number,
+	s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+): string =>
+	Array.from(Array(n))
+		.map(() => s[Math.floor(Math.random() * s.length)])
+		.join("");
--- src/lib/server/error.ts
+++ src/lib/server/error.ts
@@ -1,21 +1,21 @@
-import fs from "fs"
-import path from "path"
+import fs from "fs";
+import path from "path";
 
-import { dayExchanger } from "./day"
+import { dayExchanger } from "./day";
 
-const logFilesDir = path.join(process.cwd(), "log")
+const logFilesDir = path.join(process.cwd(), "log");
 
 export class ServerError {
-	public static readonly codes: Record<string, number> = {}
+	public static readonly codes: Record<string, number> = {};
 	constructor(public readonly content: string) {}
 }
 
 export function errorHandling<T>(error: T) {
 	if (error instanceof Error) {
 		if (error.stack === undefined) {
-			errorLog(error.message)
+			errorLog(error.message);
 		} else {
-			errorLog(error.stack)
+			errorLog(error.stack);
 		}
 	} else if (error && typeof error === "object") {
 		try {
@@ -24,14 +24,20 @@ export function errorHandling<T>(error: T) {
 			console.log(error);
 		}
 	} else {
-		errorLog(String(error))
+		errorLog(String(error));
 	}
 }
 
 function errorLog(content: string) {
-	const base = new Date().toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" })
-	let date = new Date(base)
-	let now = `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日(${dayExchanger.exchangeAbbreviation(date)}) ${date.getHours()}時${date.getMinutes()}分${date.getSeconds()}.${date.getMilliseconds()}秒`
+	const base = new Date().toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" });
+	const date = new Date(base);
+	const now = `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日(${dayExchanger.exchangeAbbreviation(date)}) ${date.getHours()}時${date.getMinutes()}分${date.getSeconds()}.${date.getMilliseconds()}秒`;
 
-	fs.appendFileSync(path.join(logFilesDir, `./${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${(date.getDate()).toString().padStart(2, "0")}.log`), (`\n----------${now}----------\n${content}\n`))
+	fs.appendFileSync(
+		path.join(
+			logFilesDir,
+			`./${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}.log`
+		),
+		`\n----------${now}----------\n${content}\n`
+	);
 }
